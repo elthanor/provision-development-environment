9ad47c1dbb9e8632eec8b1d3d8293a107eb34be9
KOUSOURAKIS Eleftherios <e.kousou@pm.me>
2023-04-10 21:24:57 +0200

    add missing script file

---
+++ install-dev-tools.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install -y vagrant
+vagrant --version
